292575799cbfa315763ec99860d22046d3858cd5
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-12-29 10:37:58 -0800

    Box

---
+++ src/Box.js
@@ -0,0 +1,25 @@
+import react, { Component } from 'react'
+import './Box.css'
+
+class Box extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { color: 'purple' }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+
+    }
+
+    render() {
+        return (
+            <div className="Box"
+                style={{ backgroundColor: this.state.color }}
+                onClick={this.handleClick}
+            />
+        )
+    }
+}
+
+export default Box
